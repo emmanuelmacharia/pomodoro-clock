c3fcda6b566166e9ebea70fbc9f206b383ede579
emmanuelmacharia <samuelmarsha@outlook.com>
2019-05-28 16:46:27 +0300

    added a footer component for the pomodoro timer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className = "app-footer">
+            <p>&copy; Emmanuel Macharia 2019</p>
+            <p>Check out my other projects on <a href = "https://github.com/emmanuelmacharia">GitHub</a></p>
+            <p>And also view my professional profile on <a href= "https://www.linkedin.com/in/emmanuel-macharia">LinkedIn</a></p>
+        </footer>
+    )
+}
