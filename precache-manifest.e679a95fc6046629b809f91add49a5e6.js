afd69f54f6573f797edea9389452bf6328a4cc03
emmanuelmacharia <samuelmarsha@outlook.com>
2019-05-29 17:39:45 +0300

    Updates

---
+++ precache-manifest.e679a95fc6046629b809f91add49a5e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd63af5d0a6f3ce49e0df745b4c0bab",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "b95d01e4c8697f6ab1df",
+    "url": "/pomodoro-clock/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "546c57bd1e7771bffb69",
+    "url": "/pomodoro-clock/static/css/main.3dca3c96.chunk.css"
+  },
+  {
+    "revision": "b95d01e4c8697f6ab1df",
+    "url": "/pomodoro-clock/static/js/2.016ba6b1.chunk.js"
+  },
+  {
+    "revision": "546c57bd1e7771bffb69",
+    "url": "/pomodoro-clock/static/js/main.3139a6c2.chunk.js"
+  },
+  {
+    "revision": "aa817215ef5e490bad59",
+    "url": "/pomodoro-clock/static/js/runtime~main.647dc629.js"
+  }
+]);
